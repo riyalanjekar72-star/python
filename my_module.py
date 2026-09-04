167b9375a9508823c73deaaf5facdef468828015
riyalanjekar72-star <riyalanjekar72@gmail.com>
2026-09-04 21:04:46 +0530

    Add files via upload

---
+++ my_module.py
@@ -0,0 +1,9 @@
+def add(a, b): 
+    return a + b 
+    
+def multiply(a, b): 
+    return a * b 
+
+    import my_module 
+    print("Addition:", my_module.add(5, 3)) 
+    print("Multiplication:", my_module.multiply(4, 2)) 
